b4f23f9bca4a4c49a55bdddacc2d4571731febca
Yarin Podoler <yarin.podler@startapp.com>
2014-08-29 14:40:03 -0400

    DEP-183 #comment Rename & added impl for context service

--- test/middleware/context.middleware.spec.js
+++ test/middleware/context.middleware.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('Unit Test Session Context MiddleWare', function(){
-    var sessionContextMiddleware = require('../../lib/middleware/session-context-middleware');
+    var sessionContextMiddleware = require('../../lib/middleware/context.middleware');
     it ('Session Context Middleware should be a function', function(){
         expect(typeof sessionContextMiddleware).toEqual('function');
     });
